f80d56423f5429914d44fcd8b18f2405723cdc7b
Achmad Ilham <30348815+achmadilham07@users.noreply.github.com>
2023-03-03 09:55:45 +0700

    Create main.dart

---
+++ shout-out/main.dart
@@ -0,0 +1,15 @@
+import "level.dart";
+
+// algoritm to find a female coder
+void main() {
+  final me = Level("Project Manager");
+  final candidates = [Level("Applicant")];
+
+  if (candidates.isNotEmpty) {
+    if (candidates.isWoman) {
+      me.happy();
+    } else {
+      me.shoutOut("Female Coders Where U At?");
+    }
+  }
+}
