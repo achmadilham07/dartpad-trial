ca309ab879b4339f14c48984f0873673577ab902
Achmad Ilham <30348815+achmadilham07@users.noreply.github.com>
2023-09-07 09:03:49 +0700

    Revert "update import file"
    This reverts commit cfbd394ee41fae82afb8ba3e1bedbbc30b9b677d.

--- dart_test/test/test.dart
+++ dart_test/test/test.dart
@@ -1,11 +1,9 @@
-import 'package:dart_test/main.dart' as mainFile;
-
 void main() {
   List<String> fail_array = [];
-  if (mainFile.a != 1) {
+  if (a != 1) {
     fail_array.add("Variabel a tidak bernilai 1");
   }
-  if (mainFile.a.runtimeType != int) {
+  if (a.runtimeType != int) {
     fail_array.add("Variabel a tidak bertipe data integer");
   }
 
