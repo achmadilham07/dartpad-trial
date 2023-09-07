cfbd394ee41fae82afb8ba3e1bedbbc30b9b677d
Achmad Ilham <30348815+achmadilham07@users.noreply.github.com>
2023-09-07 09:02:52 +0700

    update import file

--- dart_test/test/test.dart
+++ dart_test/test/test.dart
@@ -1,9 +1,11 @@
+import 'package:dart_test/main.dart' as mainFile;
+
 void main() {
   List<String> fail_array = [];
-  if (a != 1) {
+  if (mainFile.a != 1) {
     fail_array.add("Variabel a tidak bernilai 1");
   }
-  if (a.runtimeType != int) {
+  if (mainFile.a.runtimeType != int) {
     fail_array.add("Variabel a tidak bertipe data integer");
   }
 
